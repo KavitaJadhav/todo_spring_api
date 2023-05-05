0e7b3ea28f7d42289d1e2cb7e3c8c57bbdd7100e
KavitaJadhav <jadhavkavita206@gmail.com>
2023-05-05 21:05:29 +0530

    Initial commit.

---
+++ src/test/java/com/todo/todo_spring_api/TodoSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.todo_spring_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
